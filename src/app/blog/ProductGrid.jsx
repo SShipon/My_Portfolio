a2b2eca450c97c9c9284a5f718d053fdc3bf9b75
SShipon <nxshipon@gmail.com>
2024-07-12 02:51:21 +0100

    create a zoom add npm

--- src/app/blog/ProductGrid.jsx
+++ src/app/blog/ProductGrid.jsx
@@ -1,4 +1,5 @@
 "use client"
+import Zoom from 'react-reveal/Zoom';
 import { useState } from 'react';
 
 const ProductGrid = ({ products }) => {
@@ -16,7 +17,8 @@ const ProductGrid = ({ products }) => {
   return (
     <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 w-full">
       {products.map((product, index) => (
-        <div key={index} className="border rounded-lg overflow-hidden shadow-lg">
+       <Zoom>
+       <div key={index} className="border rounded-lg overflow-hidden shadow-lg">
           <img src={product.image} alt={product.name} className="w-full h-48 object-cover" />
           <div className="p-4">
             <h2 className="text-lg font-semibold">{product.name}</h2>
@@ -36,6 +38,7 @@ const ProductGrid = ({ products }) => {
             <p className="text-yellow-500 text-sm mt-2">Rating: {product.rating}</p>
           </div>
         </div>
+        </Zoom>
       ))}
     </div>
   );
